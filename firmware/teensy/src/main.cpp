f9f3aaf071448f60d23e7ea18250e3ee144752ad
Tobias Kirschstein <lev@ideegeo.com>
2017-12-09 20:13:45 -0500

    LED single color test and onboard power on LED

--- firmware/teensy/src/main.cpp
+++ firmware/teensy/src/main.cpp
@@ -1,7 +1,7 @@
 #include <OctoWS2811.h>
 
-const int ledsPerStrip = 800;
-const int NUM_LEDS = 800;
+const int ledsPerStrip = 864;
+const int NUM_LEDS = 864;
 
 DMAMEM int displayMemory[ledsPerStrip*6];
 int drawingMemory[ledsPerStrip*6];
@@ -10,22 +10,42 @@ const int config = WS2811_GRB | WS2811_800kHz;
 OctoWS2811 leds(ledsPerStrip, displayMemory, drawingMemory, config);
 
 unsigned int Color(byte r, byte g, byte b);
+byte r,g,b;
+int i;
+
+void flash_all(byte r, byte g, byte b, long ms) {
+    for (i=0; i < NUM_LEDS; i++) {
+        leds.setPixel(i, Color(r,g,b));
+    }
+    leds.show();
+    delay(ms);
+    for (i=0; i < NUM_LEDS; i++) {
+        leds.setPixel(i, Color(0,0,0));
+   }
+   leds.show();
+}
+
+void test_lights() {
+    flash_all(255,0,0,1000);
+    flash_all(0,255,0,1000);
+    flash_all(0,0,255,1000);
+}
 
 void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+
     Serial.begin(230400);
     Serial.println("This is LEDwall. engaging matrix!");
 
     leds.begin();
-    leds.show();
+    test_lights();
 }
 
 int serialGlediator() {
     while (!Serial.available()) {}
     return Serial.read();
 }
-
-byte r,g,b;
-int i;
 
 void loop() {
     while (serialGlediator() != 1) {}
