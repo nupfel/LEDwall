57aca477362eadb2064133cb945d1469d69f86a8
Tobias Kirschstein <lev@ideegeo.com>
2017-12-16 16:45:37 -0500

    turned controller from command line interface to web server

--- perl/controller.pl
+++ perl/controller.pl
@@ -1,33 +1,61 @@
 #!/usr/bin/env perl
 
 use common::sense;
+use Mojolicious::Lite;
+use File::Path 'make_path';
 use Device::SerialPort;
 use Scalar::Util 'looks_like_number';
 
-my $setting = shift @ARGV;
-my $value = shift @ARGV;
+my $LOG_DIR = '/tmp/LEDwall';
+my $PORT    = '/dev/ttyACM0';
+make_path($LOG_DIR) unless -d $LOG_DIR;
 
-my %command = (
-    brightness => 3,
-    speed => 4,
+my $wall = Device::SerialPort->new($PORT) || die "Can't open $PORT: $!";
+$wall->baudrate(230400);
+
+my %settings = (
+    brightness   => 3,
+    speed        => 4,
     trail_length => 5,
-    hue_speed => 6,
-    density => 7,
-    restart => 8,
-    stats => 9,
+    hue          => 6,
+    hue_speed    => 7,
+    density      => 8,
+    restart      => 9,
+    stats        => 10,
 );
 
-die unless exists $command{$setting};
+# routes
+get '/' => sub {
+    my $c = shift;
+    $c->redirect_to('index.html');
+};
 
-my $w = Device::SerialPort->new("/dev/tty.usbmodem3781501");
-$w->baudrate(230400);
-if ($setting =~ m/^(restart|stats)$/) {
-    $w->write(chr($command{$setting}));
-}
-else {
-    die unless looks_like_number($value);
-    $w->write(chr($command{$setting}).chr($value));
-}
-select(undef, undef, undef, 0.2);
-my ($c, $r) = $w->read(10000);
-say "$c: $r" if $c;
+get '/set/:setting' => sub {
+    my $c       = shift;
+    my $setting = $c->stash->{setting};
+    my $value   = $c->req->param('value');
+
+    say "$setting: $value";
+
+    if ($value) {
+        $wall->write(chr($settings{$setting}) . chr($value));
+    }
+    else {
+        $wall->write(chr($settings{$setting}));
+    }
+    my ($count, $raw) = $wall->read(10000);
+    $c->render(text => $raw);
+};
+
+# set custom secret
+app->secrets(['hb3u46rt34878t48bo3fqiu34fvq83498fuv5940yuvto34ty7']);
+
+# use Time::HiRes version of time() for Mojo::Log'ing
+app->log->format(
+    sub { shift; '[' . time . '] [' . shift() . '] ' . join "\n", @_, ''; });
+app->log->path("$LOG_DIR/mojo.log");
+
+# serve static files
+app->static->paths(['./public']);
+
+app->start;
